e60cb6630ff1992776de4a48e54cd8dfa2702b21
Richard Olpindo <djricky088@yahoo.com>
2026-02-07 12:53:18 -1000

    Solved e7

--- exercises/e7.ts
+++ exercises/e7.ts
@@ -1,6 +1,15 @@
 // SPACE DATA EXERCISE 7
 // Return an array of all Planets names that have moons
-export function getPlanetsWithMoons(planets) {}
+import { Planet } from "../data/data";
+export function getPlanetsWithMoons(planets: Planet[]): string[] {
+  let planetsWithMoons: string[] = [];
+  planets.forEach((planet: Planet) => {
+    if (planet.moonsCount) {
+      planetsWithMoons.push(planet.name);
+    }
+  });
+  return planetsWithMoons;
+}
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-7"
