1ec057db024fb34e41bb9e217d422e10e2308c40
Richard Olpindo <djricky088@yahoo.com>
2026-02-08 12:54:32 -1000

    Solved e12

--- exercises/e12.ts
+++ exercises/e12.ts
@@ -1,7 +1,18 @@
 // SPACE DATA EXERCISE 12
 // Return the sum of all moons for all planets
+
+import { AllData, Planet } from "../data/data";
+
 // Return example: 42
-export const allPlanetsMoonsCount = (data) => {};
+export const allPlanetsMoonsCount = (data: AllData): number => {
+  let moonCount: number = 0;
+  data.planets.forEach((planet: Planet) => {
+    if (planet.moonsCount) {
+      moonCount += planet.moonsCount;
+    }
+  });
+  return moonCount;
+};
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-12"
