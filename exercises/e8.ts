9f2b8153e69a2cf9810c252fbd7cd6e7422001c4
Richard Olpindo <djricky088@yahoo.com>
2026-02-07 19:34:07 -1000

    Solved e8

--- exercises/e8.ts
+++ exercises/e8.ts
@@ -1,7 +1,28 @@
 // SPACE DATA EXERCISE 8
 // Return a Planet by a given moon name
 //  must have destructured parameters
-export function findPlanetByMoon({ planets, moonName }) {}
+import { Planet } from "../data/data";
+export function findPlanetByMoon({
+  planets,
+  moonName,
+}: {
+  planets: Planet[];
+  moonName: string;
+}): any {
+  let planetWithMoon: Planet | undefined;
+  planets.forEach((planet: Planet) => {
+    if (planet.moons) {
+      if (
+        planet.moons.includes(
+          moonName.slice(0, 1).toUpperCase() + moonName.slice(1).toLowerCase(),
+        )
+      ) {
+        planetWithMoon = planet;
+      }
+    }
+  });
+  return planetWithMoon;
+}
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-8"
