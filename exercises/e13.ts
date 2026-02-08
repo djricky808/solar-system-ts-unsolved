155974ea884ed9715dcc8cab16a58399b7f38343
Richard Olpindo <djricky088@yahoo.com>
2026-02-08 12:59:48 -1000

    Solved e13

--- exercises/e13.ts
+++ exercises/e13.ts
@@ -2,7 +2,15 @@
 // Return the average temperature of all the Planets
 // Return example: 132.53
 
-export function getAveragePlanetsTemperature(data) {}
+import { AllData, Planet } from "../data/data";
+
+export function getAveragePlanetsTemperature(data: AllData): number {
+  let sumTemp: number = 0;
+  data.planets.forEach((planet: Planet): void => {
+    sumTemp += planet.avgTemp;
+  });
+  return sumTemp / data.planets.length;
+}
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-13"
