63d448682679817762215acd2c3f6122101133d6
Richard Olpindo <djricky088@yahoo.com>
2026-02-09 13:06:38 -1000

    Solved e16

--- exercises/e16.ts
+++ exercises/e16.ts
@@ -1,4 +1,13 @@
 // see e16.md
 
 // DO NOT USE `any` for this, you will need to use generics in order to pass both the vitest tests and the tsc compiler
-export function find(array, callback) {}
+export function find<Type>(
+  array: Type[],
+  callback: (el: Type | undefined) => boolean,
+): Type | undefined {
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      return array[i];
+    }
+  }
+}
