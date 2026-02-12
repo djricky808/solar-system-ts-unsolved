a1819c8399113b9cab8d8f1f38072c238c858279
Richard Olpindo <djricky088@yahoo.com>
2026-02-12 12:42:24 -1000

    Solved minBy of e17

--- exercises/e17.ts
+++ exercises/e17.ts
@@ -1,3 +1,11 @@
-export const minBy = (array, cb) => {};
+export const minBy = (array, cb) => {
+  let minimum;
+  for (let i = 0; i < array.length; i++) {
+    if (!minimum || cb(array[i]) < cb(minimum)) {
+      minimum = array[i];
+    }
+  }
+  return minimum;
+};
 
 export function maxBy(array, cb) {}
