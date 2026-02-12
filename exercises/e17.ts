d57da2df07bd62b9b527fb5788ebd96388449233
Richard Olpindo <djricky088@yahoo.com>
2026-02-12 13:01:12 -1000

    Added type definitions for minBy

--- exercises/e17.ts
+++ exercises/e17.ts
@@ -1,9 +1,11 @@
-export const minBy = <Type>(array: Type[], cb: (type: Type) => Type): Type => {
-  let minimum;
+export const minBy = <Type>(
+  array: Type[],
+  cb: (type: Type | undefined) => string | number,
+): Type | undefined => {
+  if (array.length == 0) return undefined;
+  let minimum = array[0];
   for (let i = 0; i < array.length; i++) {
-    if (!minimum) {
-      minimum = array[i];
-    } else if (cb(array[i]) < cb(minimum)) {
+    if (cb(array[i]) < cb(minimum)) {
       minimum = array[i];
     }
   }
