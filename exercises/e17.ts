77c6e4d92b077c20df5ec9126686f80ddf082d03
Richard Olpindo <djricky088@yahoo.com>
2026-02-12 13:05:41 -1000

    Solved maxBy with type definitions

--- exercises/e17.ts
+++ exercises/e17.ts
@@ -12,4 +12,16 @@ export const minBy = <Type>(
   return minimum;
 };
 
-export function maxBy(array, cb) {}
+export function maxBy<Type>(
+  array: Type[],
+  cb: (type: Type | undefined) => string | number,
+): Type | undefined {
+  if (array.length === 0) return undefined;
+  let maximum: Type | undefined = array[0];
+  for (let elm of array) {
+    if (cb(elm) > cb(maximum)) {
+      maximum = elm;
+    }
+  }
+  return maximum;
+}
